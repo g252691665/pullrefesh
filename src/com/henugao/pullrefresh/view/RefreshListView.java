71f4167c4d1c54655287e58c11a4ed51de135cb1
henugao <252691665@qq.com>
2016-01-11 20:08:45 +0800

    新建一个下拉刷新的工程

---
+++ src/com/henugao/pullrefresh/view/RefreshListView.java
@@ -0,0 +1,26 @@
+package com.henugao.pullrefresh.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+public class RefreshListView extends ListView {
+	
+	
+	public RefreshListView(Context context) {
+		super(context);
+		init();
+	}
+
+
+
+	public RefreshListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+	}
+
+	private void init() {
+		// TODO Auto-generated method stub
+		
+	}
+}
